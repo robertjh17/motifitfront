3c6c3118250e12b9dce27738b2294fa7213e13ca
robertjh17 <43060067+robertjh17@users.noreply.github.com>
2023-09-10 23:12:48 +0200

    verlepte gdpr

---
+++ scripts/js/gdpr.js
@@ -0,0 +1,100 @@
+class GDPR {
+
+    constructor() {
+        this.showStatus();
+        this.showContent();
+        this.bindEvents();
+
+        if(this.cookieStatus() !== 'accept') this.showGDPR();
+    }
+
+    bindEvents() {
+        let buttonAccept = document.querySelector('.gdpr-consent__button--accept');
+        let buttonReject = document.querySelector('.gdpr-consent__button--reject');
+        buttonAccept.addEventListener('click', () => {
+            this.cookieStatus('accept');
+            this.showStatus();
+            this.showContent();
+            this.hideGDPR();
+        });
+
+//student uitwerking
+        buttonReject.addEventListener('click', () => {
+            this.cookieStatus('reject');
+            this.showStatus();
+            this.showContent();
+            this.hideGDPR();
+        });
+
+
+
+
+    }
+
+    showContent() {
+        this.resetContent();
+        const status = this.cookieStatus() == null ? 'not-chosen' : this.cookieStatus();
+        const element = document.querySelector(`.content-gdpr-${status}`);
+        element.classList.add('show');
+
+    }
+
+    resetContent(){
+        const classes = [
+            '.content-gdpr-accept',
+            '.content-gdpr-reject',
+            '.content-gdpr-not-chosen'];
+
+        for(const c of classes){
+            document.querySelector(c).classList.add('hide');
+            document.querySelector(c).classList.remove('show');
+        }
+    }
+
+    showStatus() {
+        document.getElementById('content-gpdr-consent-status').innerHTML =
+            this.cookieStatus() == null ? 'Niet gekozen' : this.cookieStatus();
+    }
+
+    cookieStatus(status) {
+
+        if (status) localStorage.setItem('gdpr-consent-choice', status);
+
+//student uitwerking
+
+        return localStorage.getItem('gdpr-consent-choice');
+    }
+
+//student uitwerking
+
+
+    hideGDPR(){
+        document.querySelector(`.gdpr-consent`).classList.add('hide');
+        document.querySelector(`.gdpr-consent`).classList.remove('show');
+    }
+
+    showGDPR(){
+        document.querySelector(`.gdpr-consent`).classList.add('show');
+    }
+
+    // https://www.w3schools.com/js/js_cookies.asp
+    getCookie(cname) {
+        let name = cname + "=";
+        let decodedCookie = decodeURIComponent(document.cookie);
+        let ca = decodedCookie.split(';');
+        for(let i = 0; i <ca.length; i++) {
+            let c = ca[i];
+            while (c.charAt(0) == ' ') {
+                c = c.substring(1);
+            }
+            if (c.indexOf(name) == 0) {
+                return c.substring(name.length, c.length);
+            }
+        }
+        return "";
+    }
+
+}
+
+const gdpr = new GDPR();
+
