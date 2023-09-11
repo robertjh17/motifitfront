12318ae1b15b1684b78a93025cbd99746d6926f9
robertjh17 <43060067+robertjh17@users.noreply.github.com>
2023-09-11 15:07:59 +0200

    fontawesome toegevoegd

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
 
 ReactDOM.render(
     <React.StrictMode>
         <h1 className="text-center">Mijn React App</h1>
+        <FontAwesomeIcon icon={faCoffee} className="fs-1"/>
     </React.StrictMode>,
     document.getElementById('root')
 );
