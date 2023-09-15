492b5d593eac9c2deaa8b3c3fa48b6ef4cc568b4
robertjh17 <43060067+robertjh17@users.noreply.github.com>
2023-09-15 23:28:54 +0200

    cloudflare test

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import About from './pages/About'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+        <App />
+        <About />
   </React.StrictMode>,
 )
